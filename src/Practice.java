5538907e8bc3da2f9e3420fc2d772de60b511001
TeamFlex754 <ians.mcvay@gmail.com>
2024-08-26 17:46:58 -0700

    first commit

---
+++ src/Practice.java
@@ -0,0 +1,17 @@
+public class Practice {
+    public static void main(String[] args) {
+
+        int num1 = 25;
+
+        if (num1 < 20) {
+            System.out.println("The number is less than 20");
+
+        }
+        else if (num1 > 30) {
+            System.out.println("The number is more than 30");
+        }
+        else {
+            System.out.println("The number is between 20 and 30");
+        }
+    }
+}
