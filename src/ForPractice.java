d1c77278bd27c0d63db48d51f6cbeb1ce8ceadee
TeamFlex754 <ians.mcvay@gmail.com>
2024-09-04 18:55:23 -0700

    first commit

---
+++ src/ForPractice.java
@@ -0,0 +1,11 @@
+public class ForPractice {
+    public static void main(String[] args) {
+        int num = 10;
+
+        System.out.println("Countdown: ");
+        for (int i = num; i > 0; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Blastoff!");
+    }
+}
